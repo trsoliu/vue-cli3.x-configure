624c9566ee9344a497bff609ce8c3ec77d091918
LYDZYL <416739034@qq.com>
2018-10-29 09:25:12 +0800

    no message

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,26 @@
 // babel.config.js
 module.exports = {
-  presets: [
-    [
-      "@vue/app",
-      {
-        polyfills: ["es6.promise", "es6.symbol"]
-      }
-    ]
-  ],
-  "plugins": [
+	presets: [
+		//		  [
+		//		    "@vue/app",
+		//		    {
+		//		      polyfills: ["es6.promise", "es6.symbol"]
+		//		    }
+		//		  ]
+		//https://www.npmjs.com/package/@vue/babel-preset-app
+		['@vue/babel-preset-app', {
+			//			'target': {　　　　　　　　　　
+			//				"browser": ["ie>=11", "chrome>=65"],
+			////				"node": "8.9.0",
+			////				"safari": "tp"　　　　　　　　
+			//			},
+			//			"modules": false,
+			//			"debug": true,
+			//			"uglify": true,
+			"useBuiltIns": false
+		}]
+	],
+	"plugins": [
 		["import", {
 			"libraryName": "iview",
 			"libraryDirectory": "src/components"
